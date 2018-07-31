9880d09d5648292755d8e2ed14d7842ef25b3477
zhengziheng <33058602+zhengziheng@users.noreply.github.com>
2018-07-31 18:29:42 +0800

    import elementUI

--- demo/src/components/search.vue
+++ demo/src/components/search.vue
@@ -1,15 +1,26 @@
 <template>
-    <div>search</div>
+    <div>
+        <div class="search-wrapper">
+        <el-upload
+            class="upload-demo"
+            drag
+            action="https://jsonplaceholder.typicode.com/posts/"
+            multiple>
+            <i class="el-icon-upload"></i>
+            <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+            <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+        </el-upload>
+        </div>
+
+    </div>
 </template>
 
 
 <script>
-    export default {
-
-    }
+import elUpload from ElementUI;
+export default {};
 </script>
 
 
 <style scope lang="stylus">
-
 </style>
